571c2283a7b65fbba507519d93cc2ba3691623e0
Navneet Chauhan <navchauhan.n@gmail.com>
2023-11-07 21:57:12 +0530

    Created Signup Page with API integration.

---
+++ src/apiConnection.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const apiConnection = async (endpoint, method, payload = null) => {
+    return await axios({
+        method,
+        url: `http://127.0.0.1:8000${endpoint}`,
+        data: {
+            ...payload
+        }
+    })
+    .then(res => res)
+    .catch(err => console.log(err))
+}
+
+export default apiConnection;
---
+++ src/constant/index.js
@@ -0,0 +1,7 @@
+export const apiEndpoints = {
+    REGISTER_USER_ENDPOINT: '/create-user'
+}
+
+export const httpMethods = {
+    POST: 'POST'
+}
